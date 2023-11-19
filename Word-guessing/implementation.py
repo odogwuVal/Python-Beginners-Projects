23a215a91b913e53fb18c3c224594090f0e5a0c4
valentine <vmadu@gammax.exchange>
2023-11-19 17:08:32 +0100

    wip

--- Word-guessing/implementation.py
+++ Word-guessing/implementation.py
@@ -3,68 +3,31 @@ import random
 words = ['rainbow', 'computer', 'science', 'programming',
          'python', 'mathematics', 'player', 'condition',
          'reverse', 'water', 'board', 'geeks']
-user_name = input('Please what is your name: ')
 
-print(f"Good Luck ! {user_name}")
+system_word = random.choice(words)
 
-# Function will choose one random
-# word from this list of words
-word = random.choice(words)
+user_name = input('input your name: ')
+print(f'Goodluck {user_name}!')
 
-guesses = ''
- 
-# any number of turns can be used here
-turns = 5
+turn = 12
 
-while turns > 0:
-    # counts the number of times a user fails
+while turn > 0:
     failed = 0
+    guess = input('guess an alphabet: ')
+    if not guess:
+        print(f'you have to enter an alphabet')
+    else:
+        for char in system_word:
+            if guess in system_word:
+                print(f'Congratulations!.....{char} is in {system_word}!')
+                failed += 1
+                break
+                
+        if failed > 0:
+            break
 
-    # all characters from the input
-    # word taking one at a time.
-    for char in word:
- 
-        # comparing that character with
-        # the character in guesses
-        if char in guesses:
-            print(char, end=" ")
- 
-        else:
-            print("_")
- 
-            # for every failure 1 will be
-            # incremented in failure
-            failed += 1
+        print(f'try again. you have {turn} turns left')
 
-    if failed == 0:
-        # user will win the game if failure is 0
-        # and 'You Win' will be given as output
-        print("You Win")
- 
-        # this print the correct word
-        print("The word is: ", word)
-        break
- 
-    # if user has input the wrong alphabet then
-    # it will ask user to enter another alphabet
-    print()
-    guess = input("guess a character: ")
- 
-    # every input character will be stored in guesses
-    guesses += guess
- 
-    # check input with the character in word
-    if guess not in word:
- 
-        turns -= 1
- 
-        # if the character doesn’t match the word
-        # then “Wrong” will be given as output
-        print("Wrong")
- 
-        # this will print the number of
-        # turns left for the user
-        print("You have", + turns, 'more guesses')
- 
-        if turns == 0:
-            print("You Loose")
+        if turn == 1:
+            print(f'better luck next time....the word is {system_word}')
+    turn -= 1
